24ba02e1eebf6c028b441863292e23abf46069b6
Your Name <you@example.com>
2026-02-22 20:56:46 +0100

    Added basic configuration

--- app/src/main/java/com/dominik/control/kidshield/dashboard/MainActivity.kt
+++ app/src/main/java/com/dominik/control/kidshield/dashboard/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.dominik.control.kidshield.dashboard
 
+import android.app.Application
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,8 +13,30 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import com.dominik.control.kidshield.dashboard.ui.theme.KidShieldDashboardTheme
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
+import jakarta.inject.Inject
 
+@HiltAndroidApp
+class KidShield : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override val workManagerConfiguration: Configuration
+        get() {
+            Log.d("WM", "Using HiltWorkerFactory")
+            return Configuration.Builder()
+                .setWorkerFactory(workerFactory)
+                .setMinimumLoggingLevel(Log.INFO)
+                .build()
+        }
+}
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
